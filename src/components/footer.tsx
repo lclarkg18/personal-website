b19f2743b843a10d10db5bf5a98675190261faf3
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-05-01 03:00:39 +0200

    Create footer as separate file

---
+++ src/components/footer.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { useLocation } from 'react-router-dom';
+
+import { Trans } from 'react-i18next';
+
+import { LanguageObject } from '../types';
+
+interface FooterProps {
+  languageObject: LanguageObject
+}
+
+export default function Footer({ languageObject }: FooterProps) {
+  function getPathWithoutLng() : string {
+    const pathWithoutFirstSlash: string = useLocation().pathname.substring(1);
+    const firstSlashLocation: number = pathWithoutFirstSlash.indexOf('/');
+    if (firstSlashLocation === -1) {
+      return '';
+    }
+    return pathWithoutFirstSlash.substring(firstSlashLocation + 1);
+  }
+
+  function filterLanguages() {
+    return new Map(
+      [...languageObject.flagCodes]
+        .filter(([lang]) => lang !== languageObject.activeLanguage),
+    );
+  }
+
+  return (
+    <div className="container is-max-widescreen level">
+      <div className="level-left">
+        <div className="level-item">
+          <Trans i18nKey="footer.open-source-note">
+            <text style={{ whiteSpace: 'pre-wrap' }} />
+            <a href="https://github.com/lclarkg18/personal-website" target="_blank" rel="noreferrer" />
+          </Trans>
+        </div>
+      </div>
+      <div className="level-right">
+        {[...filterLanguages().entries()].map(([lang, flag]) => (
+          <a className="is-64x64" href={`/${lang}/${getPathWithoutLng()}`} rel="noreferrer">
+            <img
+              width={30}
+              alt={`${flag}-Flag`}
+              src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${flag}.svg`}
+            />
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+}

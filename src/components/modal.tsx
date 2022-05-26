9fcf9277e3118fe467cd3cbe41e5b6ca2f898bfd
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-05-26 18:56:21 +0100

    Create reusable modal for projects

---
+++ src/components/modal.tsx
@@ -0,0 +1,39 @@
+import React, {
+  useContext,
+} from 'react';
+import 'bulma/css/bulma.css';
+import { ModalContext } from '../providers/context';
+
+interface ModalProps {
+  title: JSX.Element,
+  content: JSX.Element,
+  modalContextDefinition: string,
+}
+
+export default function Modal({ title, content, modalContextDefinition }: ModalProps): JSX.Element {
+  // For some weird reason this CANNOT be a one-liner
+  const context = useContext(ModalContext);
+  const state = context[modalContextDefinition];
+
+  const active = state.isActive ? 'is-active' : '';
+
+  return (
+    <div className={`modal ${active}`}>
+      <div className="modal-background" />
+      <div className="modal-card">
+        <section className="modal-card-body" style={{ borderRadius: 10 }}>
+          <div className="level" style={{ paddingInline: '10px', paddingTop: '10px' }}>
+            <h1 className="title is-2">{title}</h1>
+            <button
+              onClick={() => state.setIsActive(!state.isActive)}
+              className="delete level-right is-align-self-flex-start"
+              aria-label="close"
+            />
+          </div>
+          <hr />
+          <div>{content}</div>
+        </section>
+      </div>
+    </div>
+  );
+}

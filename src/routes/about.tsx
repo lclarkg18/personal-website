67755a1f0a51d93b2ae297ef1ab64a6af01f4ef8
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-05-26 18:52:27 +0100

    Add basic content to about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { capitalise } from '../helpers';
 
 export default function About() {
+  const { t } = useTranslation();
+
   return (
     <main>
-      <h2>About</h2>
+      <h1 className="title is-1 has-text-weight-bold">{capitalise(t('navbar-sections.about'))}</h1>
+      <br />
+      <h2 className="subtitle is-4 has-text-weight-bold">TL;DR</h2>
+      <p> I'm a spanish-british CS student who loves coding, sustainable city design and </p>
     </main>
   );
 }

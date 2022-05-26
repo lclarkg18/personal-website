6719bc567e676bf26b442b9328ac4786890c3113
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-05-26 18:53:14 +0100

    Add basic content to blog page

--- src/routes/blog.tsx
+++ src/routes/blog.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { capitalise } from '../helpers';
 
 export default function Blog() {
+  const { t } = useTranslation();
+
   return (
     <main>
-      <h2>Blog</h2>
+      <h1 className="title is-1 has-text-weight-bold">{capitalise(t('navbar-sections.blog'))}</h1>
     </main>
   );
 }

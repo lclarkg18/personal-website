824a7bd543fd5eaa0ca9a0d3a21ef956a2c9a6ba
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-06-14 16:56:21 +0100

    Add animations to homepage

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import HomePageLottie from '../components/home/HomePageLottie';
+import aboutAnimation from '../animations/268-avatar-man-flat-edited.json';
+import projectsAnimation from '../animations/409-tool-outline-edited.json';
+import blogAnimation from '../animations/35-edit-outline-edited.json';
 
 import './home.css';
+import useWindowDimensions from '../miscelaneous/DimensionsHook';
 
 export default function Home() {
   const { t, i18n } = useTranslation();
+  const { height, width } = useWindowDimensions();
   const contactMeLink: string = i18n.language === 'es' ? 'mailto:work@liamclark.com?subject=Hola%20Liam!' : 'mailto:work@liamclark.com?subject=What\'s%20up%20Liam%3F';
 
+  // @ts-ignore
   return (
     <main>
       <br />
@@ -18,8 +25,14 @@ export default function Home() {
         {' '}
         Software Developer based in London.
       </h1>
+      <br />
+      <div className={`is-flex is-justify-content-center ${width > 767 ? 'is-flex-direction-row' : 'is-flex-direction-column'}`}>
+        <HomePageLottie sectionName="about" animationData={aboutAnimation} />
+        <HomePageLottie sectionName="projects" animationData={projectsAnimation} />
+        <HomePageLottie sectionName="blog" animationData={blogAnimation} />
+      </div>
       <div style={{ paddingTop: 10, display: 'flex' }}>
-        <a href={contactMeLink} style={{ color: '#5f8bce' }}>
+        <a href={contactMeLink} style={{ color: '#8db2df' }}>
           <p className="effect effect-1 effect-2">
             {' '}
             <FontAwesomeIcon icon={faEnvelope} />
@@ -29,11 +42,6 @@ export default function Home() {
           </p>
         </a>
       </div>
-      <div className="is-flex is-justify-content-center column">
-        <br />
-        <p className="subtitle"> (Beware, this webpage is under construction)</p>
-      </div>
-      <br />
     </main>
   );
 }
